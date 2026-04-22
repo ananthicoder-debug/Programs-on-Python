e9ffc49bc9cb9432ebc2c9f49c07d2ea386018aa
Anantha valli U <ananthigomathi202006@gmail.com>
2026-04-22 15:57:53 +0530

    Remove unnecessary print statements from Mthod resolution.py

--- ex9/Mthod resolution.py
+++ ex9/Mthod resolution.py
@@ -10,7 +10,3 @@ class Child(Father):
 c1=Child()
 print("Method Resolution Order:\n")
 print(Child.mro())
-[24bcs035@mepcolinux ex9]$python3 ex1.py
-Method Resolution Order:
-
-[<class '__main__.Child'>, <class '__main__.Father'>, <class '__main__.GrandFather'>, <class 'object'>]
