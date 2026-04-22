68573f1d30670afd82dbc342a5698eebd2793727
Anantha valli U <ananthigomathi202006@gmail.com>
2026-04-22 14:10:44 +0530

    Add perimeter calculation for rectangle

---
+++ ex1/Perimeter of rectangle.py
@@ -0,0 +1,4 @@
+length=int(input("Enter the length of the rectangle:"))
+breadth=int(input("Enter the breadth of the rectangle:"))
+perimeter=2*(length+breadth)
+print("Perimeter of the rectangle is " "%d" %perimeter)
