2deeff56a80c223ad1c999367ff938934066d77c
Anantha valli U <ananthigomathi202006@gmail.com>
2026-04-22 14:11:15 +0530

    Add script to swap two numbers

---
+++ ex1/Swap 2 numbers.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter a number a:"))
+n2=int(input("Enter a number b:"))
+temp=n1
+n1=n2
+n2=temp
+print("Value of a after swapping: %d" %n1)
+print("Value of b after swapping: %d" %n2)
